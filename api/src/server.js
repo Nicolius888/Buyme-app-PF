61d70d783339457eed806f71fad250e5c747d125
Nico Sammaritano <82170757+Nicolius888@users.noreply.github.com>
2022-02-23 13:03:53 -0800

    Update server.js

--- api/src/server.js
+++ api/src/server.js
@@ -6,7 +6,7 @@ const server = express();
 const cors = require('cors');
 
 
-const whiteList = ['https://buyme-app-pf.vercel.app', 'http://www.buymeapp.tk','https://vercel.com/nicolius888/buyme-app-pf/8XwrQTWhkNF3cABAQYXdUVnEtXvM']
+const whiteList = ['https://buyme-app-pf.vercel.app']
 
 
 
@@ -16,24 +16,24 @@ server.use(express.json());
 server.use(morgan("dev"));
 server.use(
   cors({
-    allowedHeaders: ["authorization", "Content-Type"], // you can change the headers
-    exposedHeaders: ["authorization"], // you can change the headers
+//     allowedHeaders: ["authorization", "Content-Type"], // you can change the headers
+//     exposedHeaders: ["authorization"], // you can change the headers
     origin: whiteList,
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    preflightContinue: false,
+//     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//     preflightContinue: false,
   })
 );
 
-server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", 'https://buyme-app-pf.vercel.app');
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
+// server.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", 'https://buyme-app-pf.vercel.app');
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+//   next();
+// });
 
 
 // Routes
